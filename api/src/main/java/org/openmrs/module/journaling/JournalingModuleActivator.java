43a0a020307fcafdc7c9129326b3e09014a8870d
Dieterich Lawson <dieterich.lawson@gmail.com>
2011-04-30 20:55:33 -0500

    first commit

---
+++ api/src/main/java/org/openmrs/module/journaling/JournalingModuleActivator.java
@@ -0,0 +1,28 @@
+package org.openmrs.module.journaling;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.Activator;
+
+/**
+ * This class contains the logic that is run every time this module is either started or shutdown
+ */
+public class JournalingModuleActivator implements Activator {
+	
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	/**
+	 * @see org.openmrs.module.Activator#startup()
+	 */
+	public void startup() {
+		log.info("Starting Basic Module");
+	}
+	
+	/**
+	 * @see org.openmrs.module.Activator#shutdown()
+	 */
+	public void shutdown() {
+		log.info("Shutting down Basic Module");
+	}
+	
+}
